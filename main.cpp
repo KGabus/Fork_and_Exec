5caa80eb249bc839630f12dd61e4d50ab0676b9d
KGabus <ktgabus@yahoo.com>
2018-02-10 19:25:37 -0500

    can copy a single hardcoded file to a new location

--- main.cpp
+++ main.cpp
@@ -4,18 +4,38 @@
 #include <fstream>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <stdio.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 
 //namespace fs = std::experimental/__cpp_lib_experimental_filesystem;
 using namespace std;
 DIR *tempDirPointer = nullptr;
 
-class ForkAndExec {
-    int main() {
-        std::cout << "Hello, World!" << std::endl;
-        string testFileLocation = "";
-        string testDestination = "";
+const int BUFFSIZE = 8192;
+//extern int errno;
 
+//class ForkAndExec {
+    int main(int argCount, char* argValues[]) {
+        std::cout << "Hello, World!" << std::endl;
+        int testFileLocation = open("startdoc", O_RDONLY, 0);
+        int testDestination = open("enddoc", O_WRONLY | O_CREAT, 0644);
+        int n;
+        char buf[BUFFSIZE];
+
+        while ((n = read(testFileLocation/*STDIN_FILENO*/, buf, BUFFSIZE)) > 0){
+            if (write(testDestination/*STDOUT_FILENO*/, buf, n) != n){
+                perror("Write Error");
+            }
+        }
+        if (n < 0){
+            perror("Read Error");
+        }
+/*
         tempDirPointer = opendir("./temp");
         if (tempDirPointer == nullptr)
         {
@@ -26,7 +46,7 @@ class ForkAndExec {
                 std::cout << "Could not create temp directory.";
             else std::cout << "temp directory created.";
             tempDirPointer = opendir("./temp");
-/*        if (tempDirPointer == nullptr)
+    /*        if (tempDirPointer == nullptr)
         {
             std::cout << "Creating /var directory";
             int dirError = mkdir("var", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
@@ -44,14 +64,16 @@ class ForkAndExec {
             }
 
         }
-*/
-        }
+    */
+/*        }
         else
         {
             std::cout << "temp directory exists";
             std::experimental::filesystem::copy(testFileLocation, testDestination);
         }
-
-        return 0;
+*/
+        exit(0);
     }
-};
+
+
+//};
