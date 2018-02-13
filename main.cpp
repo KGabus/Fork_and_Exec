bc4a3d260ba371c24de62b17e322553b4d2c5fc5
kaylee <ktgabus@gmail.com>
2018-02-12 19:23:43 -0500

    technically compiles but probably doesn't actually work. Implemented execlp to try to compile the files but that's untested

--- main.cpp
+++ main.cpp
@@ -23,151 +23,134 @@ const int BUFFSIZE = 8192;
 int n;
 char buf[BUFFSIZE];
 string compileCommand = "g++ -pass-exit-codes -std=c++14 ";
-char* commandArgs[];                                                    //todo: this could be used with execv to compile the files
+string commandArgs[20];                                                    //todo: this could be used with execv to compile the files
 
 
-//class ForkAndExec {
 
-int copyFile(char* startFileName)
-{   //copies a file to a temp folder created in the same directory is the program
-    //returns 0 if copy successful, -1 if read error, -2 if write error
+    int copyFile(char* startFileName)
+    {   //copies a file to a temp folder created in the same directory is the program
+        //returns 0 if copy successful, -1 if read error, -2 if write error
 
-    char endFileName[100] = "./temp/";
-    strcat(endFileName, startFileName);                             //create the copied file's name/path
+        char endFileName[100] = "./temp/";
+        strcat(endFileName, startFileName);                             //create the copied file's name/path
 
-    compileCommand += endFileName;
-    compileCommand += " ";
+        compileCommand += endFileName;
+        compileCommand += " ";
 
-  //  string endFileName = "./temp" + startFileName;              //todo: strip off location path here if you're going to do it
+      //  string endFileName = "./temp" + startFileName;              //todo: strip off location path here if you're going to do it
 
-    int startFile = open(startFileName, O_RDONLY, 0);
-    int endFile = open(endFileName, O_WRONLY | O_CREAT, 0644);
+        int startFile = open(startFileName, O_RDONLY, 0);
+        int endFile = open(endFileName, O_WRONLY | O_CREAT, 0644);
 
-    while ((n = read(startFile, buf, BUFFSIZE)) > 0) {
-        if (write(endFile, buf, n) != n) {
-            perror("Write Error");
-            return -2;
+        while ((n = read(startFile, buf, BUFFSIZE)) > 0) {
+            if (write(endFile, buf, n) != n) {
+                perror("Write Error");
+                return -2;
+            }
         }
+        if (n < 0) {
+            perror("Read Error");
+            return -1;
+        }
+
+
+        return 0;
     }
-    if (n < 0) {
-        perror("Read Error");
-        return -1;
-    }
 
-
-    return 0;
-}
-
-void compileFiles(string command)
-{
-    //todo: compile files method
-    int pid = 0;
-    const char* commandPointer = command.c_str();
-
-    pid = fork();
-    if (pid == 0)
+    void compileFiles(int argCount)
     {
-        //todo: this is where the compiling should happen at some point
-        system(commandPointer);
-    }
-    cout << command << endl;
+        //todo: compile files method
+        int pid = 0;
+  //      const char* commandPointer = command.c_str();
 
-}
+        const char* gppCommandArray[20];
+        gppCommandArray[0] = "g++";
+        gppCommandArray[1] = "-pass-exit-codes";
+        gppCommandArray[2] = "-std=c++14";
 
-int main(int argCount, char* argValues[])
-{
-    bool noCopyErrors = true;
-
-    char* fileName;
-
-
-    tempDirPointer = opendir("./temp");
-    if (tempDirPointer == nullptr) {
-        std::cout
-                << "Directory /temp does not exist?" << endl; //todo: verify this only gets hit when that directory doesn't exist
-
-        int dirError = mkdir("temp", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-        if (dirError == -1)
-        {
-            cout << "Could not create temp directory." << endl;
-            exit(-1);
+        for (int loopIndex = 0; loopIndex < argCount - 1; loopIndex++)
+        {   //copy files to be compiled into g++ command arg array
+            gppCommandArray[loopIndex + 3] = commandArgs[loopIndex].c_str();    //todo: this should build the command args array for execlp. In theory. Maybe
         }
-        else cout << "temp directory created." << endl;
-    }
-
-    for (int fileCounter = 1; fileCounter < argCount; fileCounter++)
-    {//todo: verify this gets all the files
 
 
-        fileName = argValues[fileCounter];   //todo: this is a bad way to convert this to a string and concat. Find a better way
-        int result = copyFile(fileName);        //todo: this is also probably bad
-
-
-
-
-        switch (result)
+        pid = fork();
+        if (pid == 0)
         {
-            case 0:
-            {
-                cout << fileName << " Copied Successfully" << endl;
-                break;
-            }
-            case -1:
-            {
-                cout << fileName << " Encountered A Read Error" << endl;
-                noCopyErrors = false;
-                break;
-            }
-            case -2:
-            {
-                cout << fileName << " Encountered A Write Error" << endl;
-                noCopyErrors = false;
-                break;
-            }
+            //todo: this is where the compiling should happen at some point
+            execlp(gppCommandArray[0], gppCommandArray[20]);                    //todo: test this a lot
+  //      system(commandPointer);                                                 //todo: use execlp instead
+    /*        if (execv("/usr/bin/cc", commandArgs) > 0)                        //todo: this doesn't currently work because of permissions or something
+                cout << "Could not compile files." << endl;
+            else cout << "File compilation successful." << endl;
+    */    }
+     //   cout << command << endl;
 
-        }
     }
 
-
-    if (noCopyErrors)
+    int main(int argCount, char* argValues[])
     {
+        bool noCopyErrors = true;
+        int fileCounter;
 
-        compileFiles(compileCommand);
-    }
+        char* fileName;
 
-/*
-    }
-            tempDirPointer = opendir("./temp");
-    /*        if (tempDirPointer == nullptr)
-        {
-            std::cout << "Creating /var directory";
-            int dirError = mkdir("var", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+
+
+        tempDirPointer = opendir("./temp");
+        if (tempDirPointer == nullptr) {
+            std::cout
+                    << "Directory /temp does not exist?" << endl; //todo: verify this only gets hit when that directory doesn't exist
+
+            int dirError = mkdir("temp", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
             if (dirError == -1)
             {
-                std::cout << "Could not create directory";
-                //todo: set this up to do some type of return when it gets broken out into a method
+                cout << "Could not create temp directory." << endl;
+                exit(-1);
             }
-            else
+            else cout << "temp directory created." << endl;
+        }
+
+        for (fileCounter = 1; fileCounter < argCount; fileCounter++)
+        {//todo: verify this gets all the files
+
+
+            fileName = argValues[fileCounter];   //todo: this is a bad way to convert this to a string and concat. Find a better way
+            int result = copyFile(fileName);        //todo: this is also probably bad
+
+            commandArgs[fileCounter - 1] = fileName;
+
+
+            switch (result)
             {
-                tempDirPointer = opendir("./var");
-                dirError = mkdir("temp", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);    //todo: doesn't currently create a sub folder
-                if (dirError == -1)
-                    std::cout << "Well that didn't work";
+                case 0:
+                {
+                    cout << fileName << " Copied Successfully" << endl;
+                    break;
+                }
+                case -1:
+                {
+                    cout << fileName << " Encountered A Read Error" << endl;
+                    noCopyErrors = false;
+                    break;
+                }
+                case -2:
+                {
+                    cout << fileName << " Encountered A Write Error" << endl;
+                    noCopyErrors = false;
+                    break;
+                }
+
             }
-
         }
-    */
-/*        }
-        else
+
+        commandArgs[fileCounter] = "\0";        //the array passed to execv must be null terminated
+
+        if (noCopyErrors)
         {
-            std::cout << "temp directory exists";
-            std::experimental::filesystem::copy(testFileLocation, testDestination);
+
+            compileFiles(argCount);
         }
-*/
-        exit(0);
-};
+            exit(0);
+    };
 
-
-
-
-//};
